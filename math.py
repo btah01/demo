2fbbefedc0f494d673dff86f4e03819b8ef34abb
Bhavesh01 <bhavesh_tahiliani@persistent.com>
2021-07-08 15:26:43 +0530

    chng in div fn

--- math.py
+++ math.py
@@ -8,7 +8,13 @@ def multiply(x,y):
     return x*y
 #local
 def divide(x,y):
+<<<<<<< HEAD
     return x+y
+=======
+    if y==0:
+        return 'Deno is 0'
+    return x/y
+>>>>>>> 3e6cb49 (chng in div fn)
 #sqa    
 def square(x):
     return x*x

ffaae8d1f8437300fe6299d8853d6dcdbb639ec0
btah01 <87119732+btah01@users.noreply.github.com>
2021-07-08 14:28:36 +0530

    initial commit math.py

---
+++ math.py
@@ -0,0 +1,13 @@
+#modified
+def add(x,y):
+    return x+y
+#sub
+def subtract(x,y):
+    return x-y   
+def multiply(x,y):
+    return x*y
+def divide(x,y):
+    return x/y
+#sqa    
+def square(x):
+    return x*x
